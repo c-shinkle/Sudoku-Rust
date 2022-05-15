c1770ffb456138d37aba3dda8171c216190a5314
Christian Shinkle <cshinkle2018@gmail.com>
2022-05-15 16:17:49 -0500

    added combo algorithm, cleaned up benchmarks

--- benches/bench.rs
+++ benches/bench.rs
@@ -4,60 +4,74 @@ extern crate test;
 
 #[cfg(test)]
 mod tests {
+    use std::panic;
+    use test::Bencher;
+
     use sudoku_rust::board::Board;
+    use sudoku_rust::combo::combo;
     use sudoku_rust::fewest_poss::fewest_poss;
     use sudoku_rust::naive::naive;
     use sudoku_rust::prev_poss::prev_poss;
-    use test::Bencher;
 
-    #[bench]
-    fn naive_hard_sudoku(b: &mut Bencher) {
+    fn setup() -> Board {
         let mut given = Board::new();
         given
             .set_board_file("./res/bench.txt")
             .expect("File to be present");
+        given
+    }
+
+    fn teardown(board: Option<Board>) {
+        if board.is_none() {
+            panic!("No solution!")
+        }
+    }
+
+    #[bench]
+    fn naive_hard_sudoku(b: &mut Bencher) {
+        let mut given = setup();
         let mut maybe_solved: Option<Board> = Option::default();
 
         b.iter(|| {
             maybe_solved = naive(&mut given);
         });
 
-        if maybe_solved.is_none() {
-            panic!("No solution!")
-        }
+        teardown(maybe_solved);
     }
 
     #[bench]
     fn fewest_poss_hard_sudoku(b: &mut Bencher) {
-        let mut given = Board::new();
-        given
-            .set_board_file("./res/bench.txt")
-            .expect("File to be present");
+        let mut given = setup();
         let mut maybe_solved: Option<Board> = Option::default();
 
         b.iter(|| {
             maybe_solved = fewest_poss(&mut given);
         });
 
-        if maybe_solved.is_none() {
-            panic!("No solution!")
-        }
+        teardown(maybe_solved);
     }
 
     #[bench]
     fn prev_poss_hard_sudoku(b: &mut Bencher) {
-        let mut given = Board::new();
-        given
-            .set_board_file("./res/bench.txt")
-            .expect("File to be present");
+        let mut given = setup();
         let mut maybe_solved: Option<Board> = Option::default();
 
         b.iter(|| {
             maybe_solved = prev_poss(&mut given);
         });
 
-        if maybe_solved.is_none() {
-            panic!("No solution!")
-        }
+        teardown(maybe_solved);
+    }
+
+    #[bench]
+    fn combo_hard_sudoku(b: &mut Bencher) {
+        let mut given = setup();
+        let mut maybe_solved: Option<Board> = Option::default();
+
+        b.iter(|| {
+            maybe_solved = combo(&mut given);
+        });
+
+        teardown(maybe_solved);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 pub mod board;
+pub mod combo;
 pub mod fewest_poss;
 pub mod naive;
 pub mod prev_poss;
